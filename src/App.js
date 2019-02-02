44d1621c611c5946bedd82441dddfa272f6efa2e
alexnsouza6 <alexnsouza6@hotmail.com>
2019-02-02 16:56:16 -0200

    feat: adding switch for unique routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import Dashboard from './containers/Dashboard';
+
+const button = () => (
+  <Button as="a" href="http://localhost:3000/api/v1/login">
+    Log in
+    {' '}
+  </Button>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={button} />
+            <Route path="/" component={Dashboard} />
+          </Switch>
+        </Router>
       </div>
     );
   }
