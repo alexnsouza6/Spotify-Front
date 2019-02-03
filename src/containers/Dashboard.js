220d63a6212e475949c8458b599d856d243cea80
alexnsouza6 <alexnsouza6@hotmail.com>
2019-02-03 02:18:09 -0200

    feat: querying artists and mapping them

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,18 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import {
-  Grid, Segment, Header, Image,
+  Grid, Segment, Header, Image, List,
 } from 'semantic-ui-react';
 import { history } from '../App';
 import '../common/stylesheets/container.css';
 import '../common/stylesheets/button.css';
 
 class Dashboard extends Component {
-  state = { user: {} };
+  state = { user: {}, artists: [] };
 
   async componentDidMount() {
-    const response = await axios.get(' https://spotify-api-artists-only.herokuapp.com/api/v1/user');
-    this.setState({ user: response.data });
+    const userResponse = await axios.get(' http://localhost:3000/api/v1/user');
+    this.setState({ user: userResponse.data });
+    const artistsResponse = await axios.get('http://localhost:3000/api/v1/artists');
+    this.setState({ artists: artistsResponse.data });
   }
 
   componentDidUpdate() {
@@ -23,7 +25,7 @@ class Dashboard extends Component {
   }
 
   render() {
-    const { user } = this.state;
+    const { user, artists } = this.state;
     if (user) {
       return (
         <Fragment>
@@ -32,14 +34,40 @@ class Dashboard extends Component {
             textAlign="center"
             className="container-fsize container-color--black"
           >
-            <Grid.Column>
-              <Segment className="container-hsize container-color--black">
+            <Grid.Column className="container-fsize">
+              <Segment className="container-fsize container-color--black">
                 <Header as="h1" size="huge" style={{ color: 'white' }}>
                   <Image circular src={require('../common/images/spotify.png')} />
                   Hello,
                   {' '}
                   {user.username}
                 </Header>
+                <Header as="h2" size="huge" style={{ color: 'white' }}>
+                  Here are some of the artists you follow:
+                </Header>
+                <List
+                  size="large"
+                  animated
+                  celled
+                  verticalAlign="middle"
+                  style={{ color: 'white', overflow: 'scroll', height: '80%' }}
+                >
+                  {artists.map(artist => (
+                    <List.Item key={artist.name}>
+                      <Image avatar src={artist.images[0].url} />
+                      <List.Content>
+                        <List.Header style={{ color: 'white' }}>{artist.name}</List.Header>
+                        <List.Description style={{ color: 'white' }}>
+                          Followers:
+                          {' '}
+                          {artist.followers.total.toLocaleString(undefined, {
+                            minimumFractionDigits: 0,
+                          })}
+                        </List.Description>
+                      </List.Content>
+                    </List.Item>
+                  ))}
+                </List>
               </Segment>
             </Grid.Column>
           </Grid>
