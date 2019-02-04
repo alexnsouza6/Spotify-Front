59d4af3e38f957135f1fa40af6958bd84707f819
alexnsouza6 <alexnsouza6@hotmail.com>
2019-02-03 22:12:51 -0200

    feat: App tests and fixing login failing tests

--- src/components/__tests__/LoginPage.test.js
+++ src/components/__tests__/LoginPage.test.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { render } from 'enzyme';
-import renderer from 'react-test-renderer';
-import LoginPage from '../LoginPage';
-import '../../setupTests';
+import React from "react";
+import { render } from "enzyme";
+import renderer from "react-test-renderer";
+import LoginPage from "../LoginPage";
+import "../../setupTests";
 
-describe('when testing w/ snapshots', () => {
-  it('renders correctly', () => {
+describe("when testing w/ snapshots", () => {
+  it("renders correctly", () => {
     const tree = renderer.create(<LoginPage />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
 
-describe('when testing html attributes', () => {
-  it('renders a button with specific href', () => {
+describe("when testing html attributes", () => {
+  it("renders a button with specific href", () => {
     const component = render(<LoginPage />);
-    const button = component.find('a');
+    const button = component.find("a");
     const redirectURL = button[0].attribs.href;
-    expect(redirectURL).toEqual('http://localhost:3000/api/v1/login');
+    expect(redirectURL).toEqual(
+      "https://spotify-api-artists-only.herokuapp.com/api/v1/login"
+    );
   });
 });
--- src/containers/__tests__/Dashboard.test.js
+++ src/containers/__tests__/Dashboard.test.js
@@ -1,48 +1,58 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import moxios from 'moxios';
-import renderer from 'react-test-renderer';
-import Dashboard from '../Dashboard';
-import '../../setupTests';
+import React from "react";
+import { shallow } from "enzyme";
+import moxios from "moxios";
+import renderer from "react-test-renderer";
+import Dashboard from "../Dashboard";
+import "../../setupTests";
 
 const userMock = {
   data: {
-    username: 'Alex Souza',
-  },
+    username: "Alex Souza"
+  }
 };
 
 const artistsMock = {
   data: [
-    { name: 'Kendrick', images: [{ url: '123123.com' }], followers: { total: 10 } },
-    { name: 'Lamar', images: [{ url: '321321.com' }], followers: { total: 20 } },
-  ],
+    {
+      name: "Kendrick",
+      images: [{ url: "123123.com" }],
+      followers: { total: 10 }
+    },
+    { name: "Lamar", images: [{ url: "321321.com" }], followers: { total: 20 } }
+  ]
 };
 
 beforeEach(() => {
   moxios.install();
-  moxios.stubRequest('http://localhost:3000/api/v1/user', {
-    status: 200,
-    response: userMock,
-  });
-  moxios.stubRequest('http://localhost:3000/api/v1/artists', {
-    status: 200,
-    response: artistsMock,
-  });
+  moxios.stubRequest(
+    "https://spotify-api-artists-only.herokuapp.com/api/v1/user",
+    {
+      status: 200,
+      response: userMock
+    }
+  );
+  moxios.stubRequest(
+    "https://spotify-api-artists-only.herokuapp.com/api/v1/artists",
+    {
+      status: 200,
+      response: artistsMock
+    }
+  );
 });
 
 afterEach(() => {
   moxios.uninstall();
 });
 
-describe('when testing states', () => {
-  it('sets user state', done => {
+describe("when testing states", () => {
+  it("sets user state", done => {
     const component = shallow(<Dashboard />);
     setTimeout(() => {
       expect(component.state().user).toEqual(userMock);
       done();
     });
   });
-  it('sets artists state', done => {
+  it("sets artists state", done => {
     const component = shallow(<Dashboard />);
     setTimeout(() => {
       expect(component.state().artists).toEqual(artistsMock);
@@ -51,11 +61,11 @@ describe('when testing states', () => {
   });
 });
 
-describe('when testing html aspects', () => {
-  it('renders list items correctly', () => {
+describe("when testing html aspects", () => {
+  it("renders list items correctly", () => {
     const component = shallow(<Dashboard />);
     setTimeout(() => {
-      expect(component.find('li').length).toEqual(artistsMock.data.length);
+      expect(component.find("li").length).toEqual(artistsMock.data.length);
     });
   });
 });
